a8929228c0f217a9429468792c276cb66f862fe4
utsi09@naver.com <utsi09@naver.com>
2024-11-20 15:52:38 +0900

    🎉 프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>React 프로그래밍 시작</h1>
+      <Header/>
     </div>
-  );
+  )
 }
 
-export default App;
+function Header() {
+  return (
+      <h1>React 프로그래밍</h1>
+  )
+}
+
+export default App

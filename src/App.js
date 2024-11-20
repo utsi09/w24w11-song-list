c5a3dce26312f4f4c959ce8092cca4afd82797ff
utsi09@naver.com <utsi09@naver.com>
2024-11-20 16:20:55 +0900

    💩 노래 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <Header/>
       <Playlist/>
       <Container/>
+      <Container2/>
+      <Container3/>
     </div>
   )
 }
@@ -37,5 +39,30 @@ function Container() {
     </div>
   )
 }
+
+function Container2() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사건의 지평선">
+        <img 
+          src="https://picsum.photos/600/150?random=2" 
+          alt="랜덤 이미지 2"/>
+        <div className="song-title">사건의 지평선</div>
+      </a>
+    </div>
+  )
+}
+
+function Container3() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사랑은 늘 도망가">
+        <img src="https://picsum.photos/600/150?random=3" alt="랜덤 이미지 3"/>
+        <div className="song-title">사랑은 늘 도망가</div>
+      </a>
+    </div>
+  )
+}
+
 
 export default App

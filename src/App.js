6e01aad7d703816a7bd783a591582ece7c77a863
utsi09@naver.com <utsi09@naver.com>
2024-11-26 17:36:26 +0900

    🚧 Lambda Expression 사용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header/>
@@ -12,13 +12,12 @@ function App() {
   )
 }
 
-function Header() {
+const Header = () => {
   return (
       <h1>React 프로그래밍</h1>
   )
 }
-
-function Playlist() {
+const Playlist = () => {
   return (
     <div className="playlist">
       프로그래밍하면서 듣고 싶은 노래
@@ -26,7 +25,7 @@ function Playlist() {
   )
 }
 
-function Container(props) {
+const Container = (props) => {
   return (
     <div className="container">
       <a href={`https://www.youtube.com/results?search_query=${props.title}`}>

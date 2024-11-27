3fd517cd2e72b836a657dacc4276cb9e12294062
utsi09@naver.com <utsi09@naver.com>
2024-11-27 16:09:44 +0900

    💄 노래 정보 배열 사용

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import Container from './Container.js'; // Container.js에서 import한 Containe
 
 const App = () => {
   const songs = [
-    "빡",
-    "사건의 지평선",
-    "사랑은 늘 도망가",
-  ];
+    {
+      id: 1,
+      title: "빡",
+      singer: "저스디스",
+      rating:1,
+      lyrics:`Ayy, okay 난 현실에 낙서하고 다니는 문제아
+      내 멋대로 그린 세상 알잖아 I cook it right
+      스튜디오 안은 부귀영화의 냄새로만 가득해
+      내 품을 떠난 일은 관심 끊어, 끊어 다음 티켓`,
+    },
+    {
+      id: 2,
+      title: "퍼즐",
+      singer: "토일",
+      rating:2,
+      lyrics:null,
+    },
+    {
+      id: 3,
+      title: "셔터",
+      singer: "유우리",
+      rating:3,
+      lyrics:null,
+    },
+  ]
 
   return (
     <div>
-      <Header />
+      <Header/>
       <Playlist
         title="프로그래밍하면서 듣고 싶은 노래"
-        listSong={songs}
-      />
+        listSong={songs}/>
     </div>
   );
 };
@@ -23,15 +43,17 @@ const Header = () => {
   return <h1>React 프로그래밍</h1>;
 };
 
-const Playlist = (props) => { // props 매개변수 추가    
+const Playlist = props => {
   return (
     <div className='playlist'>
-      <div>{props.title}</div>
-      {props.listSong.map((song, index) => (
-        <Container key={index} title={song} index={index} />
-      ))}
+      <div className="playlist">{props.title}</div>
+      {
+        props.listSong.map(song => 
+          <Container key={song.id} song={song}/>
+        )
+      }
     </div>
-  );
-};
+  )
+}
 
 export default App;
--- src/Container.js
+++ src/Container.js
@@ -1,14 +1,24 @@
 const Container = (props) => {
     return (
+    <>
       <div className="container">
-        <a href={`https://www.youtube.com/results?search_query=${props.title}`}>
+        <a href={`https://www.youtube.com/results?search_query=${props.song.title}`}
+        target="_blank" rel="noreferer">
+            
           <img 
-            src={`https://picsum.photos/600/150?random=${props.index}`} 
-            alt={`랜덤 이미지 ${props.index}`}/>
-          <div className="song-title">{props.title}</div>
+            src={`https://picsum.photos/600/150?random=${props.song.id}`} 
+            alt={`랜덤 이미지 ${props.song.id}`}/>
+          <div className="song-title">
+            {`${props.song.title} (${props.song.singer})`}
+            </div>
         </a>
-      </div>
+        </div>
+        {props.song?.lyrics && (
+          <pre className="song-lyrics">
+            {props.song.lyrics}
+          </pre>
+        )}
+      </>
     )
   }
-
   export default Container
